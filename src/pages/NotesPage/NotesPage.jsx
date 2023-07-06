9c7b873c959866f3d3009303fda1ff46927d7bc1
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-07-06 21:55:29 +0700

    add: input field and notes area with scrolling

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -3,7 +3,32 @@ import styles from './NotesPage.module.css'
 
 const NotesPage = () => {
 	return (
-		<h1>Notes!</h1>
+		<main>
+			<section className={styles['notes-area']}>
+				<div className="notes">
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+					<h2>Заметка</h2>
+				</div>
+			</section>
+			<section className={styles['input-field']}>
+				<textarea name='note' 
+					maxLength={80} cols={40} rows={4} 
+					wrap='hard' placeholder='Ваша заметка...' autoFocus/>
+				<button>Добавить</button>
+			</section>
+		</main>
 	);
 }
 
