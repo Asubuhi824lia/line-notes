ea6bc576d40bda7fa0e4b13b37824ce74dd6ba31
Asubuhi824lia <der_anastasi84@mail.ru>
2023-07-28 22:48:28 +0700

    add: sort by input

--- src/components/UI/notes/SearchBar/SearchBar.jsx
+++ src/components/UI/notes/SearchBar/SearchBar.jsx
@@ -3,20 +3,47 @@ import styles from './SearchBar.module.css'
 import search_icon from '../../../../icons/search-icon.png'
 import calendar_icon from '../../../../icons/calendar-icon.png'
 
-const SearchBar = () => {
-    const size = 40;
+import ReactDatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.module.css'
 
+import { useMemo, useState } from 'react'
+
+
+const SearchBar = ({notes}) => {
+    const [date, setDate] = useState(new Date())
+    
+    const [searchText, setSearchText]       = useState('')
+    const [isSearchText, setIsSearchText]   = useState(false)
+
+    const sortedNotes = useMemo(() => {
+        if (isSearchText) {
+            setIsSearchText(false);
+            const searched = notes.filter(note => note.text.toLowerCase().includes(searchText))
+            console.log(searched)
+            return searched
+        }
+    }, [isSearchText, notes])
+    
+    
+    const size = 40;
     return (
         <div className={styles['search-field']}>
             <img src={search_icon} className={styles.icon} width={size} height={size}/>
             <input  className={styles['search-input']} 
                     type="text" 
-                    placeholder='Поиск по истории заметок'/>
+                    placeholder='Поиск по истории заметок'
+                    onChange={e => setSearchText(e.target.value)}
+            />
             <button className={styles['choose-date-btn']}>
+                <ReactDatePicker
+                    selected={date}
+                    onChange={d => setDate(d)}
+                    className={styles['date-picker']}
+                />
                 <img src={calendar_icon} className={styles.icon} width={size} height={size}/>
             </button>
-            <button className={styles['search-btn']}>Поиск</button>
-            <button className={styles['cancel-btn']}>Отмена</button>
+            <button className={styles['search-btn']} onClick={e => setIsSearchText(true)}>Поиск</button>
+            <button className={styles['cancel-btn']}>Сброс</button>
         </div>
     )
 }
--- src/components/pages/NotesPage/NotesPage.jsx
+++ src/components/pages/NotesPage/NotesPage.jsx
@@ -9,21 +9,21 @@ import SearchBar from '../../UI/notes/SearchBar/SearchBar'
 
 const NotesPage = () => {
 	const [notes, setNotes] = useState([
-		{ id: 1, date: '08.07.2023', text: 'One more note!' },
-		{ id: 2, date: '08.07.2023', text: 'One more note!' },
-		{ id: 3, date: '08.07.2023', text: 'One more note!' },
-		{ id: 4, date: '08.07.2023', text: 'One more note!' },
-		{ id: 5, date: '08.07.2023', text: 'One more note!' },
-		{ id: 6, date: '08.07.2023', text: 'One more note!' },
-		{ id: 7, date: '08.07.2023', text: 'One more note!' },
-		{ id: 8, date: '08.07.2023', text: 'One more note!' },
-		{ id: 9, date: '08.07.2023', text: 'One more note!' },
-		{ id: 10, date: '08.07.2023', text: 'One more note!' },
-		{ id: 11, date: '08.07.2023', text: 'One more note!' },
-		{ id: 12, date: '08.07.2023', text: 'One more note!' },
-		{ id: 13, date: '08.07.2023', text: 'One more note!' },
-		{ id: 14, date: '08.07.2023', text: 'One more note!' },
-		{ id: 15, date: '08.07.2023', text: 'One more note!' },
+		{ id: 1, date: '08.07.2023', text: 'First note!' },
+		{ id: 2, date: '08.07.2023', text: 'Second note!' },
+		{ id: 3, date: '08.07.2023', text: 'Third note!' },
+		{ id: 4, date: '08.07.2023', text: 'Fourth note!' },
+		{ id: 5, date: '08.07.2023', text: 'Fifth note!' },
+		{ id: 6, date: '08.07.2023', text: 'Sixth note!' },
+		{ id: 7, date: '08.07.2023', text: 'Seventh note!' },
+		{ id: 8, date: '08.07.2023', text: 'Eighth note!' },
+		{ id: 9, date: '08.07.2023', text: 'Ninth note!' },
+		{ id: 10, date: '08.07.2023', text: 'Tenth note!' },
+		{ id: 11, date: '08.07.2023', text: 'Eleventh note!' },
+		{ id: 12, date: '08.07.2023', text: 'Twelfth note!' },
+		{ id: 13, date: '08.07.2023', text: 'Thirteenth note!' },
+		{ id: 14, date: '08.07.2023', text: 'Fourteenth note!' },
+		{ id: 15, date: '08.07.2023', text: 'Fifteenth note!' },
 	])
 	const [isInc, setIsInc] = useState(false)
 
@@ -39,9 +39,9 @@ const NotesPage = () => {
 
 	return (
 		<div className={styles['NotesSection']}>
-			<SearchBar/>
-			<NoteList notes={notes} remove={removeNote} isInc={isInc} />
-			<NoteForm notes={notes} create={createNote} />
+			<SearchBar 	notes={notes}/>
+			<NoteList 	notes={notes} remove={removeNote} isInc={isInc} />
+			<NoteForm 	notes={notes} create={createNote} />
 		</div>
 	);
 }
