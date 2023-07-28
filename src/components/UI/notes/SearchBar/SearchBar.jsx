6ecb40233eb48363a0a8e7c16424730b38798570
Asubuhi824lia <der_anastasi84@mail.ru>
2023-07-29 00:18:37 +0700

    add: sort by date

--- src/components/UI/notes/SearchBar/SearchBar.jsx
+++ src/components/UI/notes/SearchBar/SearchBar.jsx
@@ -15,14 +15,27 @@ const SearchBar = ({notes}) => {
     const [searchText, setSearchText]       = useState('')
     const [isSearchText, setIsSearchText]   = useState(false)
 
-    const sortedNotes = useMemo(() => {
+    let sortedNotes;
+    sortedNotes = useMemo(() => {
         if (isSearchText) {
             setIsSearchText(false);
             const searched = notes.filter(note => note.text.toLowerCase().includes(searchText))
-            console.log(searched)
             return searched
         }
     }, [isSearchText, notes])
+
+    sortedNotes = useMemo(() => {
+        let day = date.getDate()
+        day = day < 10 ? '0'+String(day) : day
+
+        let month = date.getMonth() + 1
+        month = month < 10 ? '0'+String(month) : month
+
+        const formed_date = day + '.' + month + '.' + date.getFullYear()
+
+        const searched = notes.filter(note => note.date == formed_date)
+        return searched
+    }, [date, notes])
     
     
     const size = 40;
@@ -30,15 +43,15 @@ const SearchBar = ({notes}) => {
         <div className={styles['search-field']}>
             <img src={search_icon} className={styles.icon} width={size} height={size}/>
             <input  className={styles['search-input']} 
-                    type="text" 
+                    type="text"
                     placeholder='Поиск по истории заметок'
                     onChange={e => setSearchText(e.target.value)}
             />
             <button className={styles['choose-date-btn']}>
                 <ReactDatePicker
+                    className={styles['date-picker']}
                     selected={date}
                     onChange={d => setDate(d)}
-                    className={styles['date-picker']}
                 />
                 <img src={calendar_icon} className={styles.icon} width={size} height={size}/>
             </button>
