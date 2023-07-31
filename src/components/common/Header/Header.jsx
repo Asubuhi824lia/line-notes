0502356678e0da27f5ff19b8979d43347024c324
Asubuhi824lia <der_anastasi84@mail.ru>
2023-07-31 23:26:07 +0700

    add: header`s links

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,19 +1,34 @@
 import styles from './Header.module.css'
 
+import notes		from '../../../icons/notes-icon.png'
 import user 		from '../../../icons/user-icon.png'
 import copyright 	from '../../../icons/copyright-icon.png'
+
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
 	return (
 		<header>
 			<nav>
-				<button className={styles['header-item']}>
-					<img src={copyright} />
-				</button>
-				<button to='/account' className={styles['header-item']}>
-					<img src={user} />
-				</button>
+				<Link to='/notes'>
+					<button className={styles['header-item']}>
+						<img src={notes} />
+					</button>
+				</Link>
+
+				<div>
+					<Link to='/auth'>
+						<button className={styles['header-item']}>
+							<img src={copyright} />
+						</button>
+					</Link>
+					<Link to='/account'>
+						<button className={styles['header-item']}>
+							<img src={user} />
+						</button>
+					</Link>
+				</div>
 			</nav>
 		</header>
 	);
