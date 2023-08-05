0c5599cfea275247b0dd1b94662558a12f4fcb2e
Asubuhi824lia <der_anastasi84@mail.ru>
2023-08-05 11:19:02 +0700

    add: edit icon to SendButton

--- src/components/UI/button/SendButton.jsx
+++ src/components/UI/button/SendButton.jsx
@@ -1,14 +1,15 @@
 import styles from './SendButton.module.css'
 
 import send_icon from '../../../icons/send-icon.png'
+import edit_icon from '../../../icons/check-mark.png'
 
 
 const SendButton = (props) => {
     return (
         <button disabled={props.isTextareaEmpty} onClick={props.onClick} className={styles['send-button']}>
             {props.isTextareaEmpty
-                ? <img src={send_icon} height={24} width={24} className={styles.disabled} />
-                : <img src={send_icon} height={24} width={24} />
+                ? <img src={props.type ? edit_icon : send_icon} height={24} width={24} className={styles.disabled} />
+                : <img src={props.type ? edit_icon : send_icon} height={24} width={24} />
             }
         </button>
     );
