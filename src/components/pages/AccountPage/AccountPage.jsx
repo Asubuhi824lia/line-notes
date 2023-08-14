10499e445a671b7bd684ad53afbaa8fa8d9f021b
Asubuhi824lia <der_anastasi84@mail.ru>
2023-08-14 23:33:15 +0700

    add: enable and focus input while edit user data

--- src/components/pages/AccountPage/AccountPage.jsx
+++ src/components/pages/AccountPage/AccountPage.jsx
@@ -1,31 +1,57 @@
+import { useState } from 'react'
 import styles from './AccountPage.module.css'
+import { useEffect } from 'react'
 
 
 const user_data = {
 	email: 'cocoa_mokoa*66@gmail.com',
 	login: 'Cocoa_Micola',
-	passw: 'JiJaJiJaJa',
+	password: 'JiJaJiJaJa',
 }
 
 
 const AccountPage = () => {
+
+	const [editData, setEditData] = useState('')
+
+	useEffect(() => {
+		switch (editData) {
+			case "email": 		
+				document.getElementById('email').focus(); 
+				break;
+			case "login": 		
+				document.getElementById('login').focus(); 
+				break;
+			case "password": 	
+				document.getElementById('password').focus(); 
+				break;
+			default: break;
+		}
+	}, [editData])
+
+	const isFocus = (type) => (editData == type)
+	const getInputClass = (type) => (editData == type) ? styles['edit-input'] : styles['info-input']
+	
 	return (
 		<section>
 			<div className={styles['data-fields']}>
 				<label>
 					<span>Почта</span>
-					<input disabled className={styles.input} value={user_data.email}/>
-					<a href='#' className={styles['auth-link']}>Изменить</a>
+					<input 	disabled={!isFocus("email")} value={user_data.email} id='email'
+							className={`${styles.input} ${getInputClass("email")}`}/>
+					<a 	href='#' className={styles['auth-link']} onClick={() => setEditData("email")}>Изменить</a>
 				</label>
 				<label>
 					<span>Логин</span>
-					<input disabled className={styles.input} value={user_data.login}/>
-					<a href='#' className={styles['auth-link']}>Изменить</a>
+					<input 	disabled={!isFocus("login")} value={user_data.login} id='login'
+							className={`${styles.input} ${getInputClass("login")}`}/>
+					<a href='#' className={styles['auth-link']} onClick={() => setEditData("login")}>Изменить</a>
 				</label>
 				<label>
 					<span>Пароль</span>
-					<input disabled className={styles.input} value={user_data.passw}/>
-					<a href='#' className={styles['auth-link']}>Изменить</a>
+					<input 	disabled={!isFocus("password")} value={user_data.password} id='password'
+							className={`${styles.input} ${getInputClass("password")}`}/>
+					<a href='#' className={styles['auth-link']} onClick={() => setEditData("password")}>Изменить</a>
 				</label>
 			</div>
 			<a href='#' className={styles['gate-link']}>Выйти</a>
