bdbbc17741ed6350a7f773348fb234898dbfd84e
Asubuhi824lia <der_anastasi84@mail.ru>
2023-08-15 20:06:01 +0700

    add: login for user

--- src/components/pages/LoginPage/LoginPage.jsx
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -1,15 +1,46 @@
 import styles from './LoginPage.module.css'
+
+import { useContext, useEffect, useState } from 'react';
+
 import { NavLink } from 'react-router-dom';
+import { AuthContext, UserContext } from '../../../context';
 
 
 const LoginPage = () => {
+
+    const [email, setEmail] 		= useState()
+	const [login, setLogin] 		= useState()
+	const [password, setPassword]	= useState()
+
+
+	const users = useContext(UserContext)
+	const setIsAuth = useContext(AuthContext)['setIsAuth']
+
+
+    const checkUser = () => {
+		const user = JSON.stringify( formUserData() )
+
+        users.forEach((u, id) => {
+            if (JSON.stringify(u) == user) {
+                setUser(id);
+                logIn();
+                return;
+            }
+        })
+    }
+
+    const formUserData = () => ({email: email, login: login, password: password})
+	const setUser 	= (id) => localStorage.setItem("user_id", id);
+	const logIn		= () => setIsAuth(true);
+
+
 	return (
 		<section className={styles['auth-form']}>
 			<div >
-				<input className={styles['input']} placeholder="Почта"/>
-				<input className={styles['input']} placeholder="Логин"/>
-				<input className={styles['input']} placeholder="Пароль"/>
-				<button className={styles['button']}>Войти</button>
+				<input className={styles['input']} placeholder="Почта" value={email} onChange={e => setEmail(e.target.value)}/>
+				<input className={styles['input']} placeholder="Логин" value={login} onChange={e => setLogin(e.target.value)}/>
+				<input className={styles['input']} placeholder="Пароль" value={password} onChange={e => setPassword(e.target.value)}/>
+				<button className={styles['button']} onClick={checkUser}>Войти</button>
 			</div>
 			<NavLink to="/auth" className={styles['gate-link']}>Зарегестрироваться</NavLink>
 		</section>
