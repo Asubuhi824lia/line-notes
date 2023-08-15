e4b0b56e3921f28134c8e802874d1b67c163f71e
Asubuhi824lia <der_anastasi84@mail.ru>
2023-08-15 16:25:52 +0700

    add: set and cancel edit of user data

--- src/components/pages/AccountPage/AccountPage.jsx
+++ src/components/pages/AccountPage/AccountPage.jsx
@@ -14,44 +14,82 @@ const AccountPage = () => {
 
 	const [editData, setEditData] = useState('')
 
+	const [email, setEmail] = useState(user_data.email)
+	const [login, setLogin] = useState(user_data.login)
+	const [password, setPassword] = useState(user_data.password)
+
 	useEffect(() => {
 		switch (editData) {
-			case "email": 		
-				document.getElementById('email').focus(); 
-				break;
-			case "login": 		
-				document.getElementById('login').focus(); 
-				break;
-			case "password": 	
-				document.getElementById('password').focus(); 
-				break;
+			case "email": document.getElementById('email').focus(); break;
+			case "login": document.getElementById('login').focus(); break;
+			case "password": document.getElementById('password').focus(); break;
 			default: break;
 		}
 	}, [editData])
 
+
+	const changeUserData = (field, newData) => { user_data[`${field}`] = newData; }
+	const resetChanges = () => { 
+		setEmail(user_data.email)
+		setLogin(user_data.login)
+		setPassword(user_data.password)
+		setEditData("")
+	}
+
+	const editField = (type) => { 
+		switch (type) {
+			case "email": user_data[`${type}`] = email; break;
+			case "login": user_data[`${type}`] = login; break;
+			case "password": user_data[`${type}`] = password; break;
+		}
+		setEditData(""); 
+	}
+
 	const isFocus = (type) => (editData == type)
 	const getInputClass = (type) => (editData == type) ? styles['edit-input'] : styles['info-input']
 	
+
 	return (
 		<section>
 			<div className={styles['data-fields']}>
 				<label>
-					<span>Почта</span>
-					<input 	disabled={!isFocus("email")} value={user_data.email} id='email'
-							className={`${styles.input} ${getInputClass("email")}`}/>
-					<a 	href='#' className={styles['auth-link']} onClick={() => setEditData("email")}>Изменить</a>
+					<span className={styles.text}>Почта</span>
+					<div>
+						<input 	disabled={!isFocus("email")} id='email' value={email} onChange={(e) => setEmail(e.target.value)}
+								className={`${styles.input} ${getInputClass("email")}`}/>
+						{isFocus("email") 
+						? <button className={`${styles['edit-btn']} ${styles.text}`} onClick={() => editField("email")}>Изменить почту</button> : ''}
+					</div>
+					{isFocus("email")
+						? <a href='#' className={styles['auth-link']} onClick={resetChanges}>Отмена</a>
+						: <a href='#' className={styles['auth-link']} onClick={() => {resetChanges(); setEditData("email")}}>Изменить</a>
+					}
 				</label>
 				<label>
-					<span>Логин</span>
-					<input 	disabled={!isFocus("login")} value={user_data.login} id='login'
-							className={`${styles.input} ${getInputClass("login")}`}/>
-					<a href='#' className={styles['auth-link']} onClick={() => setEditData("login")}>Изменить</a>
+					<span className={styles.text}>Логин</span>
+					<div>
+						<input 	disabled={!isFocus("login")} id='login' value={login} onChange={(e) => setLogin(e.target.value)}
+								className={`${styles.input} ${getInputClass("login")}`}/>
+						{isFocus("login") 
+						? <button className={`${styles['edit-btn']} ${styles.text}`} onClick={() => editField("login")}>Изменить логин</button> : ''}
+					</div>
+					{isFocus("login")
+						? <a href='#' className={styles['auth-link']} onClick={resetChanges}>Отмена</a>
+						: <a href='#' className={styles['auth-link']} onClick={() => {resetChanges(); setEditData("login")}}>Изменить</a>
+					}
 				</label>
 				<label>
-					<span>Пароль</span>
-					<input 	disabled={!isFocus("password")} value={user_data.password} id='password'
-							className={`${styles.input} ${getInputClass("password")}`}/>
-					<a href='#' className={styles['auth-link']} onClick={() => setEditData("password")}>Изменить</a>
+					<span className={styles.text}>Пароль</span>
+					<div>
+						<input 	disabled={!isFocus("password")} id='password' value={password} onChange={(e) => setPassword(e.target.value)}
+								className={`${styles.input} ${getInputClass("password")}`}/>
+						{isFocus("password") 
+						? <button className={`${styles['edit-btn']} ${styles['text']}`} onClick={() => editField("password")}>Изменить пароль</button> : ''}
+					</div>
+					{isFocus("password")
+						? <a href='#' className={styles['auth-link']} onClick={resetChanges}>Отмена</a>
+						: <a href='#' className={styles['auth-link']} onClick={() => {resetChanges(); setEditData("password")}}>Изменить</a>
+					}
 				</label>
 			</div>
 			<a href='#' className={styles['gate-link']}>Выйти</a>
