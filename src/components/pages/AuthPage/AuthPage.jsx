b81169260f9f6e9b1c80cae99b7161a979ad0224
Asubuhi824lia <der_anastasi84@mail.ru>
2023-08-22 15:06:32 +0700

    add: reduce validation code

--- src/components/pages/AuthPage/AuthPage.jsx
+++ src/components/pages/AuthPage/AuthPage.jsx
@@ -35,7 +35,7 @@ const AuthPage = () => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
-	const onSubmit = (data) => {console.log(data); console.log(login)}
+	const onSubmit = (data) => {console.log(data)}
 	
 
 	return (
@@ -44,7 +44,8 @@ const AuthPage = () => {
 				<input 
 					className={styles['input']} 
 					placeholder="Почта"
-					{...register("email", {required: true, minLength: 8, 
+					{...register("email", {required: "Не указана почта", 
+						minLength: {value: 8, message: 'Меньше 8 символов'}, 
 						pattern: {
 							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
 							message: "Некорректный адрес"
@@ -52,42 +53,38 @@ const AuthPage = () => {
 					}
 					aria-invalid={errors.email ? "true" : "false"}
 				/>
-				{errors.email?.type === "required" 	&& (<span role="alert" style={{color:'red'}}>Не указана почта</span>)}
 				{errors.email&& <span role="alert" style={{color:'red'}}>{errors.email.message}</span>}
-				{errors.email?.type === "minLength" && (<span role="alert" style={{color:'red'}}>Меньше 8 символов</span>)}
 
 				<input 
 					className={styles['input']} 
 					placeholder="Логин"	
-					{...register("login", {required: true, minLength: 2, 
+					{...register("login", {required: "Не указан логин", 
+						minLength: {value: 2, message: 'Меньше 2 символов'}, 
 						pattern: {
-							value: /[^\s]{1,}$/i,
+							value: /[^\s]+[^\s]$/i,
 							message: "Пробелы недопустимы"
 						}})
 					}
 					aria-invalid={errors.login ? "true" : "false"}
 				/>
-				{errors.login&& <span role="alert" style={{color:'red'}}>{errors.login.message}</span>}
-				{errors.login?.type === "required" && (<span role="alert" style={{color: 'red'}}>Не указан логин</span>)}
-				{errors.login?.type === "minLength" && (<span role="alert" style={{color:'red'}}>Меньше 2 символов</span>)}
+				{errors.login && <span role="alert" style={{color:'red'}}>{errors.login.message}</span>}
 
 				<input 
 					className={styles['input']} 
 					placeholder="Пароль"	
-					{...register("password", {required: true, minLength: 6, 
+					{...register("password", {required: "Не указан пароль",
+						minLength: {value: 8, message: "Меньше 6 символов"}, 
 						pattern: {
-							value: /[^\s]$/i,
+							value: /[^\s]+[^\s]$/i,
 							message: "Пробелы недопустимы"
 						}})
 					}
 					aria-invalid={errors.password ? "true" : "false"}
 				/>
 				{errors.password&& <span role="alert" style={{color:'red'}}>{errors.password.message}</span>}
-				{errors.password?.type === "required" && (<span role="alert" style={{color: 'red'}}>Не указан пароль</span>)}
-				{errors.password?.type === "minLength" && (<span role="alert" style={{color:'red'}}>Меньше 6 символов</span>)}
 
 				<button 
-					className={styles['button']} 
+					className={styles['button']}
 					// onClick={() => {regUser(); setUser(); logIn()}}
 					// onClick={() => {}}
 				>Зарегестрироваться</button>
